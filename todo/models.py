e825ff848da54b927114ddaea2ef32f879f0acd5
Jamshidbekpy <jamshidbekdev04@gmail.com>
2026-03-02 20:56:21 +0500

    added

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Todo(models.Model):
+    task = models.CharField(max_length=255)
+    completed = models.BooleanField(default=False)
+
+
+
